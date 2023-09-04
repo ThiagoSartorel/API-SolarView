d6354cdae81e044e1275d76441575442720da06a
ThiagoSartorel <thiagosartorel@uniplac.net>
2023-09-04 11:03:31 -0300

    Without HTTPS cert

--- server.ts
+++ server.ts
@@ -16,4 +16,19 @@ import { Ignitor } from '@adonisjs/core/build/standalone'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
-new Ignitor(__dirname).httpServer().start()
+new Ignitor(__dirname)
+	.httpServer()
+	.start()
+
+/*
+|--------------------------------------------------------------------------
+| AdonisJs Server
+|--------------------------------------------------------------------------
+|
+| The contents in this file is meant to bootstrap the AdonisJs application
+| and start the HTTP server to accept incoming connections. You must avoid
+| making this file dirty and instead make use of `lifecycle hooks` provided
+| by AdonisJs service providers for custom code.
+|
+*/
+
